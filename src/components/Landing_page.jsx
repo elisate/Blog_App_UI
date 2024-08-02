aec391b75ceb5151ff5135b55f9103efaa4685e0
elisate <dushimiyimanaelisa@gmail.com>
2024-08-02 21:13:12 +0200

    fix deployment

--- src/components/Landing_page.jsx
+++ src/components/Landing_page.jsx
@@ -1,30 +1,35 @@
 import React from 'react'
 import '../styles/landing.scss';
+import { CgArrowLongRight } from "react-icons/cg";
 function Landing_page() {
   const blogs = [
     {
       image: "/ro1.jpg",
       title: "hello umuganura",
-      content: "welcome welcome dear",
+      content: "welcome welcome dear blogs welcome welcome dear blogs",
     },
     {
       image: "/ro1.jpg",
       title: "hello umuganura",
-      content: "welcome welcome dear",
+      content: "welcome welcome dear blogs welcome welcome dear blogs",
     },
     {
       image: "/ro1.jpg",
       title: "hello umuganura",
-      content: "welcome welcome dear",
-    }
+      content: "welcome welcome dear blogs welcome welcome dear blogs",
+    },
   ];
   return (
     <div className='landingHolder'>
       {blogs.map((e)=>(
         <div className='contentBlogs'>
           <div><img src={e.image} className='landImage'/></div>
-          <div>{e.title}</div>
-          <div>{e.content}</div>
+          <div className='btitle'>{e.title}</div>
+          <div className='bcontent'>{e.content}</div>
+          <div className='readApp'>
+            <span>Read more</span> 
+            <CgArrowLongRight/>
+            </div>
         </div>
       ))}
     </div>
