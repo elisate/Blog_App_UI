cbddd9051caa35da19b4781f51d862be7851ca5a
elisate <dushimiyimanaelisa@gmail.com>
2024-08-05 07:29:58 +0200

    fixing bugs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      // Specify any external dependencies here if needed
+      external: ["axios", "react-hook-form"], // Add any other dependencies as necessary
+    },
+  },
+});
