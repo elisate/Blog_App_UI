411b111d82160b18563be016ff76e969d19ad713
elisate <dushimiyimanaelisa@gmail.com>
2024-08-09 00:49:01 +0200

    fix the code

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      // Specify any external dependencies here if needed
-      external: ["axios", "react-hook-form"], // Add any other dependencies as necessary
+      // Remove 'axios' from external so it gets bundled
+      external: [],
     },
   },
 });
